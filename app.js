c089fdcbf05566c9957bdf44b8dbfec4ebc3e664
Maryam Esmaili <maryam.esmaily78@gmail.com>
2022-03-28 19:22:25 +0430

    add responsive navbar

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-const toggle = document.querySelector("nav-icon");
+const sidebar = document.querySelector(".sidebar");
+const closeing = document.querySelector(".close-tag");
+const opening = document.querySelector(".nav-icon");
+
+closeing.addEventListener("click", () => {
+  closeing.classList.add("deactive");
+  sidebar.classList.add("deactive");
+  // opening.classList.toggle("deactive");
+});
+
+opening.addEventListener("click", () => {
+  closeing.classList.remove("deactive");
+  sidebar.classList.remove("deactive");
+});
