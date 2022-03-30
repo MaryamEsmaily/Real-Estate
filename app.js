f089afc4419897d87deebb1b834a2247e682ee5d
Maryam Esmaili <maryam.esmaily78@gmail.com>
2022-03-31 00:35:06 +0430

    fix slider in deals

--- app.js
+++ app.js
@@ -1,74 +1,88 @@
-const sidebar = document.querySelector(".sidebar");
-const closeing = document.querySelector(".close-tag");
-const opening = document.querySelector(".nav-icon");
-const activeLink = Array.from(document.querySelectorAll(".nav__link"));
-const houseImages = document.querySelector(".house-images");
-const delasItems = Array.from(document.querySelectorAll(".delas__items"));
-const arrow = Array.from(document.querySelectorAll(".arrow"));
-//
-closeing.addEventListener("click", () => {
-  closeing.classList.add("deactive");
-  sidebar.classList.add("deactive");
-});
+(function () {
+  function configureSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    const closeing = document.querySelector(".close-tag");
+    const opening = document.querySelector(".nav-icon");
 
-opening.addEventListener("click", () => {
-  closeing.classList.remove("deactive");
-  sidebar.classList.remove("deactive");
-});
-
-activeLink.map((i) =>
-  i.addEventListener("click", () => {
-    activeLink.forEach((element) => {
-      element.classList.remove("active");
-    });
-    delasItems.map((j) => {
-      i.classList.add("active");
-      if (i.classList[1] === j.classList[3]) {
-        j.classList.add("d-flex");
-      } else j.classList.remove("d-flex");
-    });
-  })
-);
-
-arrow.map((i) => {
-  activeLink.map((item, index) => {
-    i.addEventListener("click", () => {
-      if (
-        i.classList.contains("arrow-left") &&
-        item.classList.contains("active") &&
-        index > 0
-      ) {
-        activeLink.forEach((e) => {
-          e.classList.remove("active");
-        });
-        delasItems.map((j) => {
-          let activeTag = activeLink[index - 1];
-          activeTag.classList.add("active");
-          if (activeTag.classList[1] === j.classList[3]) {
-            j.classList.add("d-flex");
-          } else j.classList.remove("d-flex");
-        });
-      }
+    closeing.addEventListener("click", () => {
+      closeing.classList.add("deactive");
+      sidebar.classList.add("deactive");
     });
 
-    if (
-      i.classList.contains("arrow-right") &&
-      item.classList.contains("active") &&
-      index <= 4
-    ) {
-      i.addEventListener("click", () => {
-        activeLink.forEach((e) => {
-          e.classList.remove("active");
-        });
-        index += 1;
-        delasItems.map((j) => {
-          let activeTag = activeLink[index];
-          activeTag.classList.add("active");
-          if (activeTag.classList[1] === j.classList[3]) {
-            j.classList.add("d-flex");
-          } else j.classList.remove("d-flex");
-        });
-      });
+    opening.addEventListener("click", () => {
+      closeing.classList.remove("deactive");
+      sidebar.classList.remove("deactive");
+    });
+  }
+
+  function removeActiveClassLinks() {
+    const list = document.querySelectorAll(".nav__link");
+    list.forEach((item) => item.classList.remove("active"));
+  }
+  function removeActiveClassDealsItems() {
+    const list = document.querySelectorAll(".delas__items");
+    list.forEach((item) => item.classList.remove("active"));
+  }
+  function addActiveClassDealsItems(targetClass) {
+    const list = document.querySelectorAll(".delas__items");
+    list.forEach((item) => {
+      if (item.classList.contains(targetClass)) item.classList.add("active");
+    });
+  }
+  function addActiveClassLinks(value) {
+    const list = document.querySelectorAll(".nav__link");
+    list.forEach((item) => {
+      if (item.dataset.value === value) item.classList.add("active");
+    });
+  }
+
+  function activeByIndex(index) {
+    const links = Array.prototype.slice.call(
+      document.querySelectorAll(".nav__link")
+    );
+    const value = links.find((_, i) => i === index).dataset.value;
+    removeActiveClassLinks();
+    removeActiveClassDealsItems();
+    addActiveClassDealsItems(value);
+    addActiveClassLinks(value);
+    //
+  }
+
+  function handleClickArrows(dir, links) {
+    const list = Array.prototype.slice.call(links);
+    const activeTag = list.findIndex((link) =>
+      link.classList.contains("active")
+    );
+    const period = list.length;
+    //
+    const condition = dir === "right" ? activeTag < period - 1 : activeTag > 0;
+
+    if (condition) {
+      const nextActiveTag = dir === "right" ? activeTag + 1 : activeTag - 1;
+      removeActiveClassLinks();
+      list[nextActiveTag].classList.add("active");
+      activeByIndex(nextActiveTag);
     }
-  });
-});
+  }
+
+  function configureDeals() {
+    const links = document.querySelectorAll(".nav__link");
+    const dealsItems = document.querySelectorAll(".delas__items");
+    const leftArrow = document.querySelector(".arrow-left");
+    const rightArrow = document.querySelector(".arrow-right");
+    //
+    links.forEach((link, i) => {
+      link.addEventListener("click", () => {
+        activeByIndex(i);
+      });
+    });
+
+    leftArrow.addEventListener("click", () => handleClickArrows("left", links));
+
+    rightArrow.addEventListener("click", () =>
+      handleClickArrows("right", links)
+    );
+  }
+  configureDeals();
+  configureSidebar();
+})();
